5c5d440d32792c5e17d937efb7a8b553129333a1
PiachkoAliaksey <alek17y@mail.ru>
2023-06-13 21:18:08 +0300

    add: user model

---
+++ src/userModel.js
@@ -0,0 +1,15 @@
+import mongoose,{Schema} from "mongoose";
+
+
+
+const UserSchema = new Schema({
+    fullName:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    status:{type:String,required:true},
+    position:{type:String,required:true}
+}
+,{timestamps:true,});
+
+
+export default mongoose.model('UserModel',UserSchema)
