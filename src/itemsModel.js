8b0e202b559582eb6544cb472e07bf82f05de854
PiachkoAliaksey <alek17y@mail.ru>
2023-06-29 20:18:26 +0300

    add: item Schema

--- src/itemsModel.js
+++ src/itemsModel.js
@@ -4,8 +4,20 @@ import mongoose, { Schema } from "mongoose";
 
 const itemSchema = new Schema(
     {
-       
-
+        title: { type: String, required: true },
+        tags: { type: Array, default: [] },
+        collectionName:{ type: String, required: true },
+        usersLike:{ type: Array, default: [] },
+        custom3FieldOfInteger: Boolean,
+        custom3FieldOfText: Boolean,
+        custom3FieldOfLargeText: Boolean,
+        custom3FieldOfCheckBox: Boolean,
+        custom3FieldOfData: Boolean,
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'UserModel',
+            required:true,   
+        }
     },
     { timestamps: true, }
 );
