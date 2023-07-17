2062b8677a2768c769538cadac4e32ec8e2ce20a
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:49:47 +0300

    refactor: validation of collection

--- src/validations/collection.js
+++ src/validations/collection.js
@@ -2,13 +2,8 @@ import { body } from "express-validator";
 
 
 export const collectionValidation = [
-    body('title','type title').isLength({min:1}).isString(),
-    body('description','type text').isLength({min:1}).isString(),
-    body('theme','wrong format').isLength({min:1}).isString(),
-    body('custom3FieldOfInteger','boolean').optional().isBoolean(),
-    body('custom3FieldOfText','boolean').optional().isBoolean(),
-    body('custom3FieldOfLargeText','boolean').optional().isBoolean(),
-    body('custom3FieldOfText','boolean').optional().isBoolean(),
-    body('custom3FieldOfCheckBox','boolean').optional().isBoolean(),
-    body('custom3FieldOfData','boolean').optional().isBoolean(),
+    body('title', 'type title').isLength({ min: 1 }).isString(),
+    body('description', 'type text').isLength({ min: 1 }).isString(),
+    body('theme', 'wrong format').isLength({ min: 1 }).isString(),
+    body('customFields', 'boolean').optional().isArray,
 ];
