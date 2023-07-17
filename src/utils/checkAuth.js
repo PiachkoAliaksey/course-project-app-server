bde0c30e1c49f87daad38746c3df2d3bcb1af545
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:46:46 +0300

    refactor: checkAuth

--- src/utils/checkAuth.js
+++ src/utils/checkAuth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import express from 'express';
 
 
-export default (req , res, next) => {
+export default (req, res, next) => {
     const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
     if (token) {
         try {
