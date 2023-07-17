e1270cc4776ff094160ea10d64805dd70855d6cb
PiachkoAliaksey <alek17y@mail.ru>
2023-07-17 20:42:51 +0300

    refactor: custom fields for collection model

--- src/collectionModel.js
+++ src/collectionModel.js
@@ -1,26 +1,22 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 
 
 const CollectionSchema = new Schema({
-    title:{type:String,required:true},
-    description:{type:String,required:true},
-    theme:{type:String,required:true},
-    custom3FieldOfInteger:Boolean,
-    custom3FieldOfText:Boolean,
-    custom3FieldOfLargeText:Boolean,
-    custom3FieldOfCheckBox:Boolean,
-    custom3FieldOfData:Boolean,
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,   
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    theme: { type: String, required: true },
+    customFields: { type: Array, default: [] },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
     },
-    countOfItems:{
-        type:Number,
-        default:0,
+    countOfItems: {
+        type: Number,
+        default: 0,
     }
 }
-,{timestamps:true,});
+    , { timestamps: true, });
 
 
-export default mongoose.model('CollectionModel',CollectionSchema)
+export default mongoose.model('CollectionModel', CollectionSchema)
